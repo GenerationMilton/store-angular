79987ebf8c94900e07908e9fcc86608e4dd70209
Milton Muñoz <livemilton@gmail.com>
2024-07-01 12:19:47 -0500

    uso input angular

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+CommonModule
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
- img='https://picsum.photos/640/640?r=' +Math.random()
+ @Input({required:true}) img:string='';
+ @Input({required:true}) price: number=0;
+ @Input({required:true}) title: string='';
 }
