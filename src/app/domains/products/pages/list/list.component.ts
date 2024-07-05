bac9a5275ac9433d92846e9efe763ee8d5b1fd26
Milton Muñoz <livemilton@gmail.com>
2024-07-05 07:53:06 -0500

    construct the header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { Product } from '../../../shared/models/product.model';
 import { ProductComponent } from '../../components/product/product.component';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent,HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -31,9 +32,30 @@ export class ListComponent {
       },
       {
         id:Date.now(),
-        title: 'Pro 2',
+        title: 'Pro 3',
         price:100,
         image: 'https://picsum.photos/640/640?r=31',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id:Date.now(),
+        title: 'Pro 4',
+        price:200,
+        image: 'https://picsum.photos/640/640?r=32',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id:Date.now(),
+        title: 'Pro 5',
+        price:300,
+        image: 'https://picsum.photos/640/640?r=33',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id:Date.now(),
+        title: 'Pro 6',
+        price:400,
+        image: 'https://picsum.photos/640/640?r=34',
         creationAt: new Date().toISOString()
       }
     ];
