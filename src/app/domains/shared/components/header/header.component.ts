3ed261ca5aaa45ce5dd00574011259498767ff3a
Milton Muñoz <livemilton@gmail.com>
2024-07-05 22:44:57 -0500

    build car shop

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  hideSideMenu = signal(true);
+
+  toogleSideMenu(){
+    this.hideSideMenu.update(prevState=> !prevState);
+  }
 
 }
