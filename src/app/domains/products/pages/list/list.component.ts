acb526cc86e4a2a9f66047ab8322ccb41fc85ab1
Milton Muñoz <livemilton@gmail.com>
2024-07-13 10:57:50 -0500

    categories from api with a new service

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
+import { Category } from '@shared/models/category.model';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
+import { CategoryService } from '@shared/services/category.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
@@ -16,11 +18,21 @@ import { ProductService } from '@shared/services/product.service';
 export class ListComponent {
 
   products= signal<Product[]>([]);
-  //cart= signal<Product[]>([]);
+  categories= signal<Category[]>([]);
   private cartService= inject(CartService);
   private productService=inject(ProductService);
+  private categoryService=inject(CategoryService);
 
 ngOnInit(){
+  this.getProducts();
+  this.getCategories();
+}
+
+addToCart(product:Product){
+  this.cartService.addToCart(product);
+}
+
+private getProducts(){
   this.productService.getProducts()
   .subscribe({
     next:(products)=>{
@@ -31,6 +43,19 @@ ngOnInit(){
     }
   })
 }
+
+private getCategories(){
+  this.categoryService.getAll()
+  .subscribe({
+    next:(data)=>{
+        this.categories.set(data);
+    },
+    error:()=>{
+
+    }
+  })
+}
+
 
 
   //constructor(){}
@@ -81,8 +106,6 @@ ngOnInit(){
   //   this.products.set(initProducts);
   // }
 
-  addToCart(product:Product){
-    this.cartService.addToCart(product);
-  }
+
 
 }
