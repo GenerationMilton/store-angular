a6e200f2ec644f0e6c177cd726e3d0d9fd8962b0
Milton Muñoz <livemilton@gmail.com>
2024-07-13 10:18:03 -0500

    data of products, obtain data of products, and galery details

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     //provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient()]
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient()
+  ]
 };
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -1,51 +1,57 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product.model';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProductService {
-
-//   private http= inject(HttpClient);
-//   constructor() { }
-
-//   getProducts(){
-//     return this.http.get<Product[]>("https://api.escuelajs.co/api/v1/products");
-//   }
-// }
-
-@Injectable({providedIn: 'root'})
-
+@Injectable({
+  providedIn: 'root'
+})
 export class ProductService {
 
-    products= signal<Product[]>([]);
+  private http= inject(HttpClient);
+  constructor() { }
 
-    url: string = "https://api.escuelajs.co/api/v1/products";  private http = inject(HttpClient);   getProducts(){
-
-    return this.http.get<Product[]>(this.url)
-
-    .pipe(map(products => this.transformProducts(products)));
-
+  getProducts(){
+    return this.http.get<Product[]>(`https://api.escuelajs.co/api/v1/products`);
   }
 
-private transformProducts(products: Product[]): Product[] {return products.map(product => {
+  getOne(id:string){
+    return this.http.get<Product>(`https://api.escuelajs.co/api/v1/products/${id}`);
+  }
+}
 
-  // Realiza la transformación en cada producto aquí
-  const transformedImages = product.images.map(image => {let modifiedImage = image.replace(/"/g, '');
+// @Injectable({providedIn: 'root'})
 
-  // Elimina comillas dobles
+//   export class ProductService {
 
-  modifiedImage = modifiedImage.replace(/\[/g, '').replace(/\]/g, '');
+//     products= signal<Product[]>([]);
 
-  // Elimina corchetes
-  return modifiedImage;});
-  return {...product,
-  images: transformedImages
+//     url: string = "https://api.escuelajs.co/api/v1/products";  private http = inject(HttpClient);   getProducts(){
 
-  };
+//     return this.http.get<Product[]>(this.url)
 
-});
+//     .pipe(map(products => this.transformProducts(products)));
 
-}}
+
+
+//   }
+
+//     private transformProducts(products: Product[]): Product[] {return products.map(product => {
+
+//       // Realiza la transformación en cada producto aquí
+//     const transformedImages = product.images.map(image => {let modifiedImage = image.replace(/"/g, '');
+
+//       // Elimina comillas dobles
+
+//     modifiedImage = modifiedImage.replace(/\[/g, '').replace(/\]/g, '');
+
+//       // Elimina corchetes
+//       return modifiedImage;});
+//       return {...product,
+//       images: transformedImages
+
+//       };
+
+//     });
+
+//   }
+// }
