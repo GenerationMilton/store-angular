b2aabb044df0d3a4f62fbee8a9d575d934ca315a
Milton Muñoz <livemilton@gmail.com>
2024-07-14 13:50:44 -0500

    lazzyLoading applied

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
 
-import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 import { LayoutsComponent } from '@shared/components/layouts/layouts.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
-import { ListComponent } from './domains/products/pages/list/list.component';
 
 export const routes: Routes = [
 
@@ -14,15 +11,17 @@ export const routes: Routes = [
     children:[
         {
           path:'',
-          component:ListComponent
+          loadComponent:() => import('./domains/products/pages/list/list.component').then(m=> m.ListComponent)
         },
         {
           path:'about',
-          component:AboutComponent
+          loadComponent:() => import('./domains/info/pages/about/about.component').then(p=>p.AboutComponent)
+
         },
         {
           path:'product/:id',
-          component:ProductDetailComponent
+          loadComponent:() => import('./domains/products/pages/product-detail/product-detail.component').then(d=>d.ProductDetailComponent)
+
         }
     ]
   },
